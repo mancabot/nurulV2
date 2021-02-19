b86cb4fc119d1354aadce6da8409ef60dc7ba474
mancabot <78895007+mancabot@users.noreply.github.com>
2021-02-19 13:17:03 +0700

    Add files via upload

---
+++ src/toinmenu.js
@@ -0,0 +1,17 @@
+const toinmenu = (prefix, pushname) => {
+    return `◪ *Comandos do Toin*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall
+    ├─ ❏╔═╦╗───╔═╗───╔╗─
+    ├─ ❏║║║║╔╦╗║╬║╔╦╗║║─
+    ├─ ❏║║║║║║║║╗╣║║║║╚╗
+    ├─ ❏╚╩═╝╚═╝╚╩╝╚═╝╚═╝
+    ├─ ❏────────────────`
+
+}
+
+exports.toinmenu = toinmenu
